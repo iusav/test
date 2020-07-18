eaa34e8fc395ebe3ffbe1ab2bc99be5bf811aa12
Ivan <ivan.berezkin.s@gmail.com>
2020-07-18 09:54:01 +0000

    new changes

--- aisite/aisite/views.py
+++ aisite/aisite/views.py
@@ -1,4 +1,19 @@
 from django.shortcuts import redirect
+from django.shortcuts import render
+from django.conf import settings
+from django.core.files.storage import FileSystemStorage
+
+def model_form_upload(request):
+    if request.method == 'POST':
+        form = DocumentForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = DocumentForm()
+    return render(request, 'core/model_form_upload.html', {
+        'form': form
+    })
 
 def redirect_view(request):
     return redirect("/landpage")
--- aisite/landpage/views.py
+++ aisite/landpage/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from django.shortcuts import render
+from django.conf import settings
+from django.core.files.storage import FileSystemStorage
 
+def model_form_upload(request):
+    if request.method == 'POST':
+        form = DocumentForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = DocumentForm()
+    return render(request, 'core/model_form_upload.html', {
+        'form': form
+    })
 
 def index(request):
     return render(request, 'landpage/index.html')
