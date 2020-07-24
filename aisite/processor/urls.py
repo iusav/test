674585d709a955c3a14fb71e1b78de29a5f6a36c
Ivan <ivan.berezkin.s@gmail.com>
2020-07-24 22:31:31 +0000

    image upload is working now!

--- aisite/processor/urls.py
+++ aisite/processor/urls.py
@@ -1,7 +1,9 @@
 from django.urls import include, path
+from django.conf.urls import url
 
 from . import views
 
 urlpatterns = [
     path('', views.index, name = 'index'),
+    url('removeBack',views.removeBack,name='removeBack')
 ]
--- aisite/processor/views.py
+++ aisite/processor/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from django.core.files.storage import FileSystemStorage
 
 
 # Create your views here.
 def index(request):
+    return render(request, 'processor/index.html')
+
+def removeBack(request):
+    fileObj=request.FILES['filePath']
+    fs=FileSystemStorage()
+    filePathName=fs.save(fileObj.name,fileObj)
+    filePathName=fs.url(filePathName)
     return render(request, 'processor/index.html')
